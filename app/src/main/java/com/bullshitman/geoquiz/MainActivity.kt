96300328a07131b1651195163cb2b28110b5f219
Bullshitman <sokol.joe@gmail.com>
2020-05-11 22:01:13 +0300

    Challenge_Part2

--- app/src/main/java/com/bullshitman/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bullshitman/geoquiz/MainActivity.kt
@@ -2,14 +2,17 @@ package com.bullshitman.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
-    private lateinit var nextButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
     private lateinit var questionTextView: TextView
     private val questonBank = listOf(
         Question(R.string.question_australia, true),
@@ -26,7 +29,8 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
         questionTextView = findViewById(R.id.question_text_view)
-
+        prevButton = findViewById(R.id.previous_button)
+        showHide(prevButton)
         trueButton.setOnClickListener {
             checkAnswer(true)
         }
@@ -38,11 +42,20 @@ class MainActivity : AppCompatActivity() {
             currentIndex = (currentIndex + 1) % questonBank.size
             updateQuestion()
         }
+        questionTextView.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questonBank.size
+            updateQuestion()
+        }
+        prevButton.setOnClickListener {
+            currentIndex = (currentIndex - 1) % questonBank.size
+            updateQuestion()
+        }
         updateQuestion()
     }
     private fun updateQuestion() {
         val questionTextResId = questonBank[currentIndex].textResID
         questionTextView.setText(questionTextResId)
+        showHide(prevButton)
     }
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = questonBank[currentIndex].answer
@@ -52,5 +65,12 @@ class MainActivity : AppCompatActivity() {
             R.string.incorrect_toast
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
+    private fun showHide(view:View) {
+        view.visibility = if (currentIndex == 0){
+            View.INVISIBLE
+        } else{
+            View.VISIBLE
+        }
     }
 }
