b5538cfc04addb12f5402d88df70993d27f8689d
Bullshitman <sokol.joe@gmail.com>
2020-05-12 13:17:42 +0300

    Chapter3

--- app/src/main/java/com/bullshitman/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bullshitman/geoquiz/MainActivity.kt
@@ -2,9 +2,12 @@ package com.bullshitman.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
@@ -21,6 +24,7 @@ class MainActivity : AppCompatActivity() {
     private var currentIndex = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
@@ -40,6 +44,32 @@ class MainActivity : AppCompatActivity() {
         }
         updateQuestion()
     }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
     private fun updateQuestion() {
         val questionTextResId = questonBank[currentIndex].textResID
         questionTextView.setText(questionTextResId)
