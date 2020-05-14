da499a569a234a13affb4437b8a0de56c7fe5df7
Bullshitman <sokol.joe@gmail.com>
2020-05-14 13:28:58 +0300

    Chapter4

--- app/src/main/java/com/bullshitman/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bullshitman/geoquiz/MainActivity.kt
@@ -1,35 +1,29 @@
 package com.bullshitman.geoquiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.security.keystore.KeyNotYetValidException
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 
 
 private const val TAG = "MainActivity"
-private const val KEY_INDEX = "KEY_INDEX"
+private const val KEY_INDEX = "index"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
     private lateinit var questionTextView: TextView
-    private lateinit var quizViewModel: QuizViewModel
-    private lateinit var quizViewModelFactory: QuizViewModelFactory
-
+    private val quizViewModel: QuizViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
-//        currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
-        quizViewModelFactory = QuizViewModelFactory()
-        quizViewModel = ViewModelProvider(this@MainActivity, quizViewModelFactory).get(QuizViewModel::class.java)
+        val currentIndex = savedInstanceState?.getInt(KEY_INDEX, 0) ?: 0
+        quizViewModel.currentIndex = currentIndex
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
@@ -49,9 +43,10 @@ class MainActivity : AppCompatActivity() {
         updateQuestion()
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt(KEY_INDEX, quizViewModel.currentIndex)
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        Log.i(TAG, "OnSaveInstanceState called")
+        savedInstanceState.putInt(KEY_INDEX, quizViewModel.currentIndex)
     }
 
     override fun onResume() {
